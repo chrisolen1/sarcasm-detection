9cc11f7fb77c3c51bb8095f8c4ffb81492d10dbe
chrisolen1 <chrisolen@sarc-cluster-m.us-central1-f.c.sarcasm-5.internal>
2020-03-24 22:54:17 +0000

    model util and model progress

--- model_utils.py
+++ model_utils.py
@@ -1,9 +1,12 @@
 import pyspark
-
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
+
+import tensorflow as tf
+import tensorflow_hub as thub
+import bert
 
 import pandas as pd
 
@@ -11,7 +14,8 @@ import re
 
 import random
 
-
+import os
+from tqdm import tqdm
 
 def init_bert(bert_version="https://tfhub.dev/tensorflow/bert_en_wwm_cased_L-24_H-1024_A-16/1", trainable=True, do_lower_case=False):
 
@@ -23,12 +27,13 @@ def init_bert(bert_version="https://tfhub.dev/tensorflow/bert_en_wwm_cased_L-24_
 
 	bert_layer = thub.KerasLayer(bert_version,trainable=trainable)
 	vocabulary_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+	BertTokenizer = bert.bert_tokenization.FullTokenizer
 	tokenizer = BertTokenizer(vocabulary_file, do_lower_case=do_lower_case)
 
 	return bert_layer, tokenizer
 
 
-def init_spark(dyanmic_allocation="True", executor_cores="2")
+def init_spark(dynamic_allocation="True", executor_cores="2"):
 
 	"""
 	Initialize Spark context
@@ -36,13 +41,33 @@ def init_spark(dyanmic_allocation="True", executor_cores="2")
 	Returns: Spark session object
 	"""
 
-	config = pyspark.SparkConf().setAll([("spark.dynamicAllocation.enabled","True"),
-                                    ("spark.executor.cores","2")])
+	config = pyspark.SparkConf().setAll([("spark.dynamicAllocation.enabled",dynamic_allocation),
+                                    ("spark.executor.cores",executor_cores)])
 	sc = SparkContext(conf=config)
 	spark = SparkSession(sc)
 
-	return spark
+	return sc, spark
 
+def load_data(spark_context, bucket_name, dataset):
+    
+    """
+    Takes in the name of csv file in GCS and the name of the GCS bucket
+    and loads it in as a Spark dataframe
+
+    Returns: Spark df of only sarcastic, Spark df of only non-sacrastic, and the ratio 
+    between the two fo them 
+    """
+    
+    sarc = spark_context.read.csv("gs://{}/{}.csv".format(bucket_name, dataset), 
+                          inferSchema=True, header=False, sep = '\t')
+    sarc = sarc.withColumnRenamed('_c0','label').withColumnRenamed('_c1','subreddit').withColumnRenamed('_c2','context')
+    sarcastic = sarc.where(col('label')==1)
+    non_sarcastic = sarc.where(col('label')==0)
+    sarc_cnt = sarcastic.count()
+    non_sarc_cnt = non_sarcastic.count()
+    ratio = sarc_cnt / non_sarc_cnt
+    
+    return sarcastic, non_sarcastic, ratio
 
 def tokenize_sample(context):
     
@@ -59,28 +84,7 @@ def tokenize_sample(context):
     
     return ids
 
-def load_data(bucket_name, dataset):
-    
-    """
-    Takes in the name of csv file in GCS and the name of the GCS bucket
-    and loads it in as a Spark dataframe
-
-    Returns: Spark df of only sarcastic, Spark df of only non-sacrastic, and the ratio 
-    between the two fo them 
-    """
-    
-    sarc = spark.read.csv("gs://{}/{}.csv".format(bucket_name, dataset), 
-                          inferSchema=True, header=False, sep = '\t')
-    
-    sarcastic = sarc.where(col('label')==1)
-    non_sarcastic = sarc.where(col('label')==0)
-    sarc_cnt = sarcastic.count()
-    non_sarc_cnt = non_sarcastic.count()
-    ratio = sarc_cnt / non_sarc_cnt
-    
-    return sarcastic, non_sarcastic, ratio
-
- def generate_epoch_df(sarcastic, non_sarcastic, ratio, n_epochs):
+def generate_epoch_df(sarcastic, non_sarcastic, ratio, n_epochs):
     
     """
     Generates a Pandas dataframe of equal label distribution over which 
