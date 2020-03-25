c10bf95defbf4cdc7b76fb36c4c2d04936cd024a
chrisolen1 <chrisolen@sarc-cluster-m.us-central1-c.c.sarcasm-5.internal>
2020-03-25 02:31:24 +0000

    asdfasdf

--- model_utils.py
+++ model_utils.py
@@ -9,6 +9,7 @@ import tensorflow_hub as thub
 import bert
 
 import pandas as pd
+import numpy as np
 
 import re
 
@@ -55,17 +56,21 @@ def load_data(spark_context, bucket_name, dataset):
     and loads it in as a Spark dataframe
 
     Returns: Spark df of only sarcastic, Spark df of only non-sacrastic, and the ratio 
-    between the two fo them 
+    between the two of them 
     """
     
     sarc = spark_context.read.csv("gs://{}/{}.csv".format(bucket_name, dataset), 
-                          inferSchema=True, header=False, sep = '\t')
+                          inferSchema=True, header=False, sep = ',')
     sarc = sarc.withColumnRenamed('_c0','label').withColumnRenamed('_c1','subreddit').withColumnRenamed('_c2','context')
-    sarcastic = sarc.where(col('label')==1)
-    non_sarcastic = sarc.where(col('label')==0)
+    sarcastic = sarc.where(F.col('label')==1)
+    non_sarcastic = sarc.where(F.col('label')==0)
     sarc_cnt = sarcastic.count()
     non_sarc_cnt = non_sarcastic.count()
     ratio = sarc_cnt / non_sarc_cnt
+    
+    # dropping subreddit column before returning
+    sarcastic = sarcastic.drop('subreddit')
+    non_sarcastic = non_sarcastic.drop('subreddit')
     
     return sarcastic, non_sarcastic, ratio
 
@@ -91,14 +96,22 @@ def generate_epoch_df(sarcastic, non_sarcastic, ratio, n_epochs):
     we can perform mini-batch gradient descent. Each generated df is
     to be iterator over multiple times during training
     """
-    
     number = 0
     while number < n_epochs:
         non_sarc_samp = non_sarcastic.sample(ratio) # making label dist equal
-        epoch_df = sarcastic.union(non_sarc_samp)
-        X = np.array(epoch_df.select('context'))
-        y = np.array(epoch_df.select('label'))
         
+        # combine sampled non_sarcastic and whole sarcastic
+        epoch_df = sarcastic.union(non_sarc_samp)
+        
+        # tokenize context column via spark udf
+        tokenize_sample_udf = F.udf(tokenize_sample, ArrayType(IntegerType()))
+        epoch_df = epoch_df.withColumn("tokens", tokenize_sample_udf(epoch_df.context))
+        
+        # split into X and y numpy arrays
+        X = np.array(epoch_df.select('tokens').collect())
+        y = np.array(epoch_df.select('label').collect())
+        
+        # yield one call at a time
         yield X, y
         number += 1
 
